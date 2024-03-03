3956a89c5ffa62a02cedaafb2774379a6b1e7b4a
Martin Spassov <mspassov@uwo.ca>
2024-03-03 09:44:24 -0500

    Completed register and sign in pages

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -1,10 +1,75 @@
 import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../../assets/svg/visibilityIcon.svg";
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>SignIn Page</h1>
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={loginInfo.email}
+            autoComplete="on"
+            onChange={(e) =>
+              setLoginInfo((prevState) => ({
+                ...prevState,
+                email: e.target.value,
+              }))
+            }
+          />
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="passwordInput"
+              id="password"
+              autoComplete="on"
+              value={loginInfo.password}
+              onChange={(e) =>
+                setLoginInfo((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+            />
+            <img
+              src={visibilityIcon}
+              alt="Show Password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to="/reset-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="white" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
+        <Link to="/signup" className="registerLink">
+          Don't have an account? Sign up!
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,10 +1,93 @@
 import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../../assets/svg/visibilityIcon.svg";
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [registerInfo, setLoginInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>SignUp Page</h1>
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type="text"
+            className="nameInput"
+            placeholder="Full Name"
+            id="name"
+            autoComplete="on"
+            value={registerInfo.name}
+            onChange={(e) =>
+              setLoginInfo((prevState) => ({
+                ...prevState,
+                name: e.target.value,
+              }))
+            }
+          />
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            autoComplete="on"
+            value={registerInfo.email}
+            onChange={(e) =>
+              setLoginInfo((prevState) => ({
+                ...prevState,
+                email: e.target.value,
+              }))
+            }
+          />
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="passwordInput"
+              id="password"
+              autoComplete="on"
+              value={registerInfo.password}
+              onChange={(e) =>
+                setLoginInfo((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+            />
+            <img
+              src={visibilityIcon}
+              alt="Show Password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to="/reset-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="signUpBar">
+            <p className="signUpText">Sign Up</p>
+            <button className="signUpButton">
+              <ArrowRightIcon fill="white" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
+        <Link to="/signin" className="registerLink">
+          Sign In Instead!
+        </Link>
+      </div>
+    </>
   );
 };
 
