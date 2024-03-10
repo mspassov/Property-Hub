978f50e47463bb60b4a606bedabf3e98f6fe85f9
Martin Spassov <mspassov@uwo.ca>
2024-03-09 22:30:26 -0500

    Added protected routes, and forgot password functionality

--- src/components/pages/ForgotPassword.jsx
+++ src/components/pages/ForgotPassword.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import { ReactComponent as ArrowrightIcon } from "../../assets/svg/keyboardArrowRightIcon.svg";
 
 const ForgotPassword = () => {
+  const [emailInput, setEmailInput] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, emailInput);
+      toast.success("Email Sent. Please check your mailbox!");
+    } catch (error) {
+      toast.error("Could not complete request. Please try again!");
+    }
+  };
+
   return (
-    <div>
-      <h2>Forgot Password Page</h2>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="emailInput"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+            placeholder="Email"
+            id="email"
+          />
+          <Link className="forgotPasswordLink" to="/login">
+            Sign In
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowrightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 };
