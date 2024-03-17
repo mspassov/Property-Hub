fa32e8395271ca028f83f9bbe7f92c7e4ffdb915
Martin Spassov <mspassov@uwo.ca>
2024-03-16 20:28:03 -0400

    Added swiper to listing page

--- src/components/pages/Listing.jsx
+++ src/components/pages/Listing.jsx
@@ -7,6 +7,11 @@ import { getAuth } from "firebase/auth";
 import Spinner from "../Spinner";
 import shareIcon from "../../assets/svg/shareIcon.svg";
 import { Map, Marker } from "pigeon-maps";
+import SwiperCore from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
@@ -48,6 +53,23 @@ const Listing = () => {
 
   return (
     <main>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div
+                className="swiperSlideDiv"
+                style={{
+                  background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                  backgroundSize: "cover",
+                  height: "60vh",
+                }}
+              ></div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
       <div className="shareIconDiv" onClick={handleShare}>
         <img src={shareIcon} alt="Share" />
       </div>
