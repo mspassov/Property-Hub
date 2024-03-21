e4f43dab233ef748428c7bc109f545eda29f1ff6
Martin Spassov <mspassov@uwo.ca>
2024-03-20 20:02:00 -0400

    Added editing capabilities

--- src/components/pages/EditListing.jsx
+++ src/components/pages/EditListing.jsx
@@ -1,9 +1,432 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { doc, getDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase.config";
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import Spinner from "../Spinner";
 
 const EditListing = () => {
-  return (
-    <div>EditListing</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState({});
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 0,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    lat: 0,
+    long: 0,
+  });
 
-export default EditListing
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    offer,
+    regularPrice,
+    discountedPrice,
+    images,
+    latitude,
+    longitude,
+  } = formData;
+
+  const auth = getAuth();
+  const params = useParams();
+  const navigate = useNavigate();
+
+  //Fetch the listing that needs to be edited
+  useEffect(() => {
+    setLoading(true);
+
+    const getListing = async () => {
+      const docRef = doc(db, "listings", params.id);
+      const listingSnapshot = await getDoc(docRef);
+
+      if (listingSnapshot.exists()) {
+        setListing(listingSnapshot.data());
+        setFormData({
+          ...listingSnapshot.data(),
+          address: listingSnapshot.data().location,
+        });
+        setLoading(false);
+      } else {
+        navigate("/profile");
+        toast.error("Listing does not exist. Plesae try again!");
+      }
+    };
+
+    getListing();
+  }, []);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setFormData({ ...formData, userRef: user.uid });
+      } else {
+        navigate("/login");
+      }
+    });
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (+discountedPrice >= +regularPrice) {
+      setLoading(false);
+      toast.error(
+        "Discounted price needs to be less than regular price. Please try again!"
+      );
+      return;
+    }
+    if (images > 6) {
+      setLoading(false);
+      toast.error("Cannot upload more than 6 images. Please try again!");
+      return;
+    }
+
+    let geolocationObj = {};
+    let location;
+
+    const res = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_KEY}`
+    );
+    const data = await res.json();
+
+    geolocationObj.lat = data.results[0]?.geometry.location.lat ?? 0;
+    geolocationObj.lng = data.results[0]?.geometry.location.lng ?? 0;
+    location =
+      data.results.length > 0 ? data.results[0]?.formatted_address : address;
+
+    //Prep images for upload to Firebase
+    const prepImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.email}-${uuidv4()}`;
+        const storageRef = ref(storage, "images/" + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        //From Google Firebase documentation
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((img) => prepImage(img))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("Could not upload images. Must be under 2MB each");
+      return;
+    });
+
+    const listingObj = {
+      type,
+      name,
+      bedrooms,
+      bathrooms,
+      parking,
+      furnished,
+      location,
+      offer,
+      regularPrice,
+      discountedPrice,
+      geolocation: geolocationObj,
+      timestamp: serverTimestamp(),
+      imageUrls: [...imgUrls],
+      userRef: auth.currentUser.uid,
+    };
+
+    if (offer == false) {
+      delete listingObj.discountedPrice;
+    }
+
+    //upload listing to Firebase
+    const docRef2 = doc(db, "listings", params.id);
+    await updateDoc(docRef2, listingObj);
+    toast.success("Listing created successfully!");
+
+    navigate(`/category/${listingObj.type}/${docRef2.id}`);
+
+    setLoading(false);
+  };
+
+  const onMutate = (e) => {
+    let boolean = null;
+    if (e.target.value == "true") {
+      boolean = true;
+    }
+    if (e.target.value == "false") {
+      boolean = false;
+    }
+
+    //Handle Files
+    if (e.target.files) {
+      setFormData((prevState) => ({ ...prevState, images: e.target.files }));
+    }
+
+    //Handle all other inputs
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="profile">
+      <header>
+        <p className="pageHeader">Edit a property listing:</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label className="formLabel">Sell / Rent</label>
+          <div className="formButtons">
+            <button
+              type="button"
+              className={type === "sale" ? "formButtonActive" : "formButton"}
+              id="type"
+              value="sale"
+              onClick={onMutate}
+            >
+              Sell
+            </button>
+            <button
+              type="button"
+              className={type === "rent" ? "formButtonActive" : "formButton"}
+              id="type"
+              value="rent"
+              onClick={onMutate}
+            >
+              Rent
+            </button>
+          </div>
+
+          <label className="formLabel">Name</label>
+          <input
+            className="formInputName"
+            type="text"
+            id="name"
+            value={name}
+            onChange={onMutate}
+            maxLength="32"
+            minLength="10"
+            required
+          />
+
+          <div className="formRooms flex">
+            <div>
+              <label className="formLabel">Bedrooms</label>
+              <input
+                className="formInputSmall"
+                type="number"
+                id="bedrooms"
+                value={bedrooms}
+                onChange={onMutate}
+                min="0"
+                max="50"
+                required
+              />
+            </div>
+            <div>
+              <label className="formLabel">Bathrooms</label>
+              <input
+                className="formInputSmall"
+                type="number"
+                id="bathrooms"
+                value={bathrooms}
+                onChange={onMutate}
+                min="1"
+                max="50"
+                required
+              />
+            </div>
+          </div>
+
+          <label className="formLabel">Parking spot</label>
+          <div className="formButtons">
+            <button
+              className={parking ? "formButtonActive" : "formButton"}
+              type="button"
+              id="parking"
+              value={true}
+              onClick={onMutate}
+              min="1"
+              max="50"
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !parking && parking !== null ? "formButtonActive" : "formButton"
+              }
+              type="button"
+              id="parking"
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          <label className="formLabel">Furnished</label>
+          <div className="formButtons">
+            <button
+              className={furnished ? "formButtonActive" : "formButton"}
+              type="button"
+              id="furnished"
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !furnished && furnished !== null
+                  ? "formButtonActive"
+                  : "formButton"
+              }
+              type="button"
+              id="furnished"
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          <label className="formLabel">Address</label>
+          <textarea
+            className="formInputAddress"
+            type="text"
+            id="address"
+            value={address}
+            onChange={onMutate}
+            required
+          />
+
+          <label className="formLabel">Offer</label>
+          <div className="formButtons">
+            <button
+              className={offer ? "formButtonActive" : "formButton"}
+              type="button"
+              id="offer"
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !offer && offer !== null ? "formButtonActive" : "formButton"
+              }
+              type="button"
+              id="offer"
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          <label className="formLabel">Regular Price</label>
+          <div className="formPriceDiv">
+            <input
+              className="formInputSmall"
+              type="number"
+              id="regularPrice"
+              value={regularPrice}
+              onChange={onMutate}
+              min="50"
+              max="750000000"
+              required
+            />
+            {type === "rent" && <p className="formPriceText">$ / Month</p>}
+          </div>
+
+          {offer && (
+            <>
+              <label className="formLabel">Discounted Price</label>
+              <input
+                className="formInputSmall"
+                type="number"
+                id="discountedPrice"
+                value={discountedPrice}
+                onChange={onMutate}
+                min="50"
+                max="750000000"
+                required={offer}
+              />
+            </>
+          )}
+
+          <label className="formLabel">Images</label>
+          <p className="imagesInfo">Upload up to 6 images</p>
+          <input
+            className="formInputFile"
+            type="file"
+            id="images"
+            onChange={onMutate}
+            max="6"
+            accept=".jpg,.png,.jpeg"
+            multiple
+            required
+          />
+          <button type="submit" className="primaryButton createListingButton">
+            Update Listing
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default EditListing;
